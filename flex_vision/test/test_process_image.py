8dcb2050b99dddeeb2ff5d232d9a23f975315085
TaekedeHaan <taeke.dehaan@live.nl>
2022-11-29 23:08:08 +0100

    [vision] Determine True positives

--- flex_vision/test/test_process_image.py
+++ flex_vision/test/test_process_image.py
@@ -33,15 +33,16 @@ class TestProcessImage(unittest.TestCase):
         # generate background image
         background_image = np.tile(np.array(util.background_color, ndmin=3, dtype=np.uint8), (1080, 1920, 1))
 
-        # figsize = (background_image.shape[0]/20, background_image.shape[1]/20)
-        # plt.figure(figsize=figsize, dpi=20)
-        # plt.imshow(background_image)
+        dpi = 120
+        figsize = (background_image.shape[1]/dpi, background_image.shape[0]/dpi)
 
         # plot truss
-        util.plot_image(background_image)
+        plt.figure(figsize=figsize, dpi=dpi)
+        plt.imshow(background_image)
         util.plot_truss(tomato=tomatoes, peduncle=peduncle)
         util.clear_axis()
         image = util.figure_to_image(plt.gcf())
+        print(image.shape)
 
         # process the generated image
         process_image.add_image(image, px_per_mm=px_per_mm, name='test')
@@ -49,26 +50,49 @@ class TestProcessImage(unittest.TestCase):
         features_prediction = process_image.get_object_features()
 
         self.assertEqual(len(features_prediction['tomato']['centers']), 2)
-        self.assertAlmostEqual(features_prediction['tomato']['centers'][0][0], 757.1025790183744)
-        self.assertAlmostEqual(features_prediction['tomato']['centers'][0][1], 247.03665196688155)
-        self.assertAlmostEqual(features_prediction['tomato']['centers'][1][0], 828.5812539598362)
-        self.assertAlmostEqual(features_prediction['tomato']['centers'][1][1], 524.2131282930893)
-
         self.assertEqual(len(features_prediction['peduncle']['ends']), 2)
-        self.assertAlmostEqual(features_prediction['peduncle']['ends'][0][0], 655.9700969384756)
-        self.assertAlmostEqual(features_prediction['peduncle']['ends'][0][1], 460.9249898171696)
-        self.assertAlmostEqual(features_prediction['peduncle']['ends'][1][0], 935.3054573725246)
-        self.assertAlmostEqual(features_prediction['peduncle']['ends'][1][1], 292.38788289059414)
-
         self.assertEqual(len(features_prediction['peduncle']['junctions']), 2)
-        self.assertAlmostEqual(features_prediction['peduncle']['junctions'][0][0], 768.514894424575)
-        self.assertAlmostEqual(features_prediction['peduncle']['junctions'][0][1], 395.8814001344585)
-        self.assertAlmostEqual(features_prediction['peduncle']['junctions'][1][0], 823.8057402629113)
-        self.assertAlmostEqual(features_prediction['peduncle']['junctions'][1][1], 348.89286571933405)
 
-        self.assertAlmostEqual(features_prediction['grasp_location']['xy'][0], 791.7370199859157)
-        self.assertAlmostEqual(features_prediction['grasp_location']['xy'][1], 376.4717920906744)
-        self.assertAlmostEqual(features_prediction['grasp_location']['angle'], -3.842480599372535)
+        # analyze results
+        i_prediction, i_label, false_pos, false_neg = index_true_positives(
+            tomatoes['centers'],
+            features_prediction['tomato']['centers'],
+            10,
+            px_per_mm
+        )
+
+        # We expect two true positives, and zero false positives and negatives.
+        self.assertEqual(len(i_prediction), 2)
+        self.assertEqual(len(false_pos), 0)
+        self.assertEqual(len(false_neg), 0)
+
+        centers_prediction = np.array(features_prediction['tomato']['centers'])[i_prediction]
+        radii_prediction = np.array(features_prediction['tomato']['radii'])[i_prediction]
+
+        self.assertAlmostEqual(centers_prediction[0][0], 712.4411785210777)
+        self.assertAlmostEqual(centers_prediction[0][1], 236.37336778915875)
+        self.assertAlmostEqual(centers_prediction[1][0], 783.4833019827396)
+        self.assertAlmostEqual(centers_prediction[1][1], 471.47550877060104)
+
+        self.assertAlmostEqual(radii_prediction[0], 116.59999847)
+        self.assertAlmostEqual(radii_prediction[1], 147.3999939)
+
+        print(features_prediction['peduncle']['ends'])
+        print(features_prediction['peduncle']['junctions'])
+
+        self.assertAlmostEqual(features_prediction['peduncle']['ends'][0][0], 619.7435385686285)
+        self.assertAlmostEqual(features_prediction['peduncle']['ends'][0][1], 425.93113615343884)
+        self.assertAlmostEqual(features_prediction['peduncle']['ends'][1][0], 879.998726386269)
+        self.assertAlmostEqual(features_prediction['peduncle']['ends'][1][1], 270.4536869397198)
+
+        self.assertAlmostEqual(features_prediction['peduncle']['junctions'][0][0], 724.4119756581771)
+        self.assertAlmostEqual(features_prediction['peduncle']['junctions'][0][1], 367.54005569391)
+        self.assertAlmostEqual(features_prediction['peduncle']['junctions'][1][0], 774.8082578751593)
+        self.assertAlmostEqual(features_prediction['peduncle']['junctions'][1][1], 333.8174410457796)
+
+        self.assertAlmostEqual(features_prediction['grasp_location']['xy'][0], 749.9502396991849)
+        self.assertAlmostEqual(features_prediction['grasp_location']['xy'][1], 350.312256363416)
+        self.assertAlmostEqual(features_prediction['grasp_location']['angle'], -3.7234445123887827)
 
 
 def generate_truss_features(truss_center, radius_minimum, radius_maximum, angle_deg=30.0):
